785616d63cb35fa45db3fef00115c222e61be427
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:26:08 +0200

    Rendező konstruktor és getter/setter megírása

--- src/main/java/hu/filmest/filmest/classes/Rendezo.java
+++ src/main/java/hu/filmest/filmest/classes/Rendezo.java
@@ -1,4 +1,27 @@
 package hu.filmest.filmest.classes;
 
 public class Rendezo {
+    private int id;
+    private String rendezoNev;
+
+    public Rendezo(int id, String rendezoNev) {
+        this.id = id;
+        this.rendezoNev = rendezoNev;
+    }
+
+    public Rendezo(String rendezoNev) {
+        this.rendezoNev = rendezoNev;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRendezoNev() {
+        return rendezoNev;
+    }
+
+    public void setRendezoNev(String rendezoNev) {
+        this.rendezoNev = rendezoNev;
+    }
 }
