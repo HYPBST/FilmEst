b208cff4d29751d3cf8ba9591b557a9d50573a8e
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:35:54 +0200

    FilmRendezői osztály konstruktor és getter/setter megírása

--- src/main/java/hu/filmest/filmest/pivot/FilmRendezoi.java
+++ src/main/java/hu/filmest/filmest/pivot/FilmRendezoi.java
@@ -1,4 +1,19 @@
 package hu.filmest.filmest.pivot;
 
 public class FilmRendezoi {
+    private int filmId;
+    private int rendezoId;
+
+    public FilmRendezoi(int filmId, int rendezoId) {
+        this.filmId = filmId;
+        this.rendezoId = rendezoId;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public int getRendezoId() {
+        return rendezoId;
+    }
 }
