4162014c3d9268fcc0f675f9352a1cd086a141a8
Gergő <s.gergo.az@gmail.com>
2022-04-17 14:06:46 +0200

    SzineszFelvetel view és controller elkészítése

--- src/main/java/hu/filmest/filmest/controllers/SzineszFelvetel.java
+++ src/main/java/hu/filmest/filmest/controllers/SzineszFelvetel.java
@@ -1,4 +1,55 @@
 package hu.filmest.filmest.controllers;
 
-public class SzineszFelvetel {
+import hu.filmest.filmest.Controller;
+import hu.filmest.filmest.Api;
+import hu.filmest.filmest.classes.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SzineszFelvetel extends Controller {
+    @FXML
+    public TextField felvetelSzinesz;
+
+
+
+    public void initialize(){
+        felvetelSzinesz.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\sa-zA-Z*")) {
+                felvetelSzinesz.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+            }
+        });
+    }
+
+    public void onFelvetelButtonClick(ActionEvent actionEvent) {
+        String szinesz=felvetelSzinesz.getText().trim();
+        if(szinesz.isEmpty()){
+            alert("A színész megadása kötelező");
+            return;
+        }
+
+        try {
+            List<Szinesz> szineszList= Api.getSzineszekList();
+            for (Szinesz sz:szineszList
+            ) {
+                if (sz.getSzineszNev().equalsIgnoreCase(szinesz)){
+                    alert("A rendező már létezik");
+
+                    return;
+                }
+            }
+            Szinesz ujSzinesz =new Szinesz(szinesz);
+            Szinesz letrehozott= Api.szineszHozzaadas(ujSzinesz);
+            if (letrehozott != null ){
+                alert("Színész hozzáadása sikeres");
+            } else {
+                alert("Színész hozzáadása sikeretelen");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
