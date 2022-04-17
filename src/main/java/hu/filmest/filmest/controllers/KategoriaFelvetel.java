cccc857be9df680575d05857e352e45de40dbdbe
Gergő <s.gergo.az@gmail.com>
2022-04-17 14:01:29 +0200

    kategoriafelvétel kontroller elkészítés

--- src/main/java/hu/filmest/filmest/controllers/KategoriaFelvetel.java
+++ src/main/java/hu/filmest/filmest/controllers/KategoriaFelvetel.java
@@ -1,4 +1,53 @@
 package hu.filmest.filmest.controllers;
 
-public class KategoriaFelvetel {
+import hu.filmest.filmest.Controller;
+import hu.filmest.filmest.Api;
+import hu.filmest.filmest.classes.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+public class KategoriaFelvetel extends Controller {
+    @FXML
+    private TextField felvetelKategoria;
+    public void initialize(){
+        felvetelKategoria.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\sa-zA-Z*")) {
+                felvetelKategoria.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+            }
+        });
+    }
+    public void onFelvetelButtonClick(ActionEvent actionEvent) {
+        String kategoria=felvetelKategoria.getText().trim();
+        if(kategoria.isEmpty()){
+            alert("A kategória megadása kötelező");
+            return;
+        }
+
+        try {
+            List<Kategoria> kategoriaList= Api.getKategoriaList();
+            for (Kategoria k:kategoriaList
+            ) {
+                System.out.println(k.getKategoria());
+                System.out.println(kategoria);
+                if (k.getKategoria().equalsIgnoreCase(kategoria)){
+                    alert("A kategória már létezik");
+
+                    return;
+                }
+            }
+            Kategoria ujKategoria =new Kategoria(kategoria);
+            Kategoria letrehozott= Api.kategoriaHozzaadas(ujKategoria);
+            if (letrehozott != null ){
+                alert("Kategória hozzáadása sikeres");
+            } else {
+                alert("Kategória hozzáadása sikeretelen");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
