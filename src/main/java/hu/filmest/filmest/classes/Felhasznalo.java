e428e03f4f2b561ab7df8312f5830a546b345342
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:46:26 +0200

    Felhasználó osztéky konstruktor és getter

--- src/main/java/hu/filmest/filmest/classes/Felhasznalo.java
+++ src/main/java/hu/filmest/filmest/classes/Felhasznalo.java
@@ -1,4 +1,20 @@
 package hu.filmest.filmest.classes;
 
 public class Felhasznalo {
+    private int id;
+    private String email;
+
+    public Felhasznalo(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
