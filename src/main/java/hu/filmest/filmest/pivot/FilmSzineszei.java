8a03bcc3743e27f19833c22e465b48f3b2233a17
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:36:12 +0200

    FilmSzineszei osztály konstruktor és getter/setter megírása

--- src/main/java/hu/filmest/filmest/pivot/FilmSzineszei.java
+++ src/main/java/hu/filmest/filmest/pivot/FilmSzineszei.java
@@ -1,4 +1,19 @@
 package hu.filmest.filmest.pivot;
 
 public class FilmSzineszei {
+    private int filmId;
+    private int szineszId;
+
+    public FilmSzineszei(int filmId, int szineszId) {
+        this.filmId = filmId;
+        this.szineszId = szineszId;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public int getSzineszId() {
+        return szineszId;
+    }
 }
