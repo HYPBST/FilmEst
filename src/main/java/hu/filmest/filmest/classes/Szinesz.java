b7de144e9f716b04ea13b71f1a33a2602c7f142e
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:26:48 +0200

    Színész konstruktor és getter/setter megírása

--- src/main/java/hu/filmest/filmest/classes/Szinesz.java
+++ src/main/java/hu/filmest/filmest/classes/Szinesz.java
@@ -1,4 +1,28 @@
 package hu.filmest.filmest.classes;
 
 public class Szinesz {
+    private int id;
+    private String szineszNev;
+
+    public Szinesz(int id, String szineszNev) {
+        this.id = id;
+        this.szineszNev = szineszNev;
+    }
+
+    public Szinesz(String szineszNev) {
+        this.szineszNev = szineszNev;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getSzineszNev() {
+        return szineszNev;
+    }
+
+    public void setSzineszNev(String szineszNev) {
+        this.szineszNev = szineszNev;
+    }
 }
