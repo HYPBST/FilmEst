7ef0d0988dd56331ccbfaf1fc6f777dbe6dcc477
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:27:37 +0200

    Színész konstruktor és getter/setter megírása

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module hu.filmest.filmest {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
-    opens hu.filmest.filmest to javafx.fxml;
+    opens hu.filmest.filmest to javafx.fxml, com.google.gson;
     exports hu.filmest.filmest;
     exports hu.filmest.filmest.controllers;
-    opens hu.filmest.filmest.controllers to javafx.fxml;
+    opens hu.filmest.filmest.controllers to com.google.gson,javafx.fxml;
+    opens hu.filmest.filmest.classes to com.google.gson, javafx.fxml;
+    opens hu.filmest.filmest.pivot to com.google.gson, javafx.fxml;
 }
