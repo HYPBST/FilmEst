56951011dfce5b42081423ad6ce4a867bafea142
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:22:37 +0200

    ApiError megírása

--- src/main/java/hu/filmest/filmest/ApiError.java
+++ src/main/java/hu/filmest/filmest/ApiError.java
@@ -1,4 +1,14 @@
 package hu.filmest.filmest;
 
 public class ApiError {
+    String message;
+
+    public ApiError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
