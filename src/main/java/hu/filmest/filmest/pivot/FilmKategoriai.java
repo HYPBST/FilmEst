b6efb9ef9a3e5910fd4253d03f2ccb794258c05c
Gergő <s.gergo.az@gmail.com>
2022-04-17 13:35:34 +0200

    FilmKategóriái osztály konstruktor és getter/setter megírása

--- src/main/java/hu/filmest/filmest/pivot/FilmKategoriai.java
+++ src/main/java/hu/filmest/filmest/pivot/FilmKategoriai.java
@@ -1,4 +1,19 @@
 package hu.filmest.filmest.pivot;
 
 public class FilmKategoriai {
+    private int filmId;
+    private int kategoriaId;
+
+    public FilmKategoriai(int filmId, int kategoriaId) {
+        this.filmId = filmId;
+        this.kategoriaId = kategoriaId;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public int getKategoriaId() {
+        return kategoriaId;
+    }
 }
