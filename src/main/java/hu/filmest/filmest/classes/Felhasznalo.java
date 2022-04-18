d1f4cafe0cf1d4a83bd1680a37bef420cf48d373
Gergő <s.gergo.az@gmail.com>
2022-04-19 00:07:17 +0200

    Felesleges sorok törlése

--- src/main/java/hu/filmest/filmest/classes/Felhasznalo.java
+++ src/main/java/hu/filmest/filmest/classes/Felhasznalo.java
@@ -3,22 +3,16 @@ package hu.filmest.filmest.classes;
 public class Felhasznalo {
     private int id;
     private String email;
-    private String password;
     private int permission;
 
-    public Felhasznalo(int id, String email,String password,int permission) {
+    public Felhasznalo(int id, String email,int permission) {
         this.id = id;
         this.email = email;
-        this.password=password;
         this.permission = permission;
     }
 
     public int isAdmin() {
         return permission;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public int getId() {
