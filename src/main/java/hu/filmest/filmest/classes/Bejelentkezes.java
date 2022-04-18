05b4f6fb87bbf163f72ceee29358ebbfb067c47d
Gergő <s.gergo.az@gmail.com>
2022-04-18 15:30:32 +0200

    Admin autentikáció és login

---
+++ src/main/java/hu/filmest/filmest/classes/Bejelentkezes.java
@@ -0,0 +1,11 @@
+package hu.filmest.filmest.classes;
+
+public class Bejelentkezes {
+    String email;
+    String password;
+
+    public Bejelentkezes(String email, String jelszo) {
+        this.email = email;
+        this.password = jelszo;
+    }
+}
